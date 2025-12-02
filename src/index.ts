3a1b102bf15e5d2a8f5d364623c6c669b5f50407
p4bl1sk1 <pablogerez2000@gmail.com>
2025-12-02 13:32:39 -0300

    init bot

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import { Client, GatewayIntentBits } from "discord.js";
+import "dotenv/config";
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.on("ready", () => {
+  console.log(`Bot logueado como ${client.user?.tag}`);
+});
+
+client.on("messageCreate", (msg) => {
+  if (msg.author.bot) return;
+
+  if (msg.content === "!ping") {
+    msg.reply("Pong!");
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
